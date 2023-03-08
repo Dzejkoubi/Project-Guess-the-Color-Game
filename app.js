56ec35ee00b40df6ea77e3ec1ffdbdde2c60f394
Dzejkoubi <kubakraus06@gmail.com>
2023-03-08 15:45:46 +0100

    Kamo uz me to sere proc to nefunguje

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 let colors = [];
+let colorText = document.querySelectorAll(".color-text");
+let correctAnswerNumber = Math.floor(Math.random() * colors.length);
+let correctAnswerIndex = colors[correctAnswerNumber].slice(4, -1);
+
 for (let i = 0; i < 4; i++) {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
   colors.push(`rgb(${red}, ${green}, ${blue})`);
 }
-console.log(colors);
 for (let i = 1; i <= 4; i++) {
   document.documentElement.style.setProperty(`--color-${i}`, colors[i - 1]);
 }
 document.addEventListener("DOMContentLoaded", function () {
-  let whatColor = document.getElementById("color-display");
-  whatColor.textContent = "This is the new text.";
+  colorText.textContent = `What color is ${correctAnswerIndex}`;
 });
+
+//Console Logs
+console.log(correctAnswerNumber);
+console.log(correctAnswerIndex);
