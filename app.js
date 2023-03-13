6475a9f2aa8b6d101023f9a113f561fb8859f5f3
Dzejkoubi <kubakraus06@gmail.com>
2023-03-13 22:03:49 +0100

    Button click correct control done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let colors = [];
-let colorText = document.querySelectorAll(".color-text");
-let correctAnswerNumber = Math.floor(Math.random() * colors.length);
-let correctAnswerIndex = colors[correctAnswerNumber].slice(4, -1);
+let colorText = document.getElementById("color-display");
+let colorButton0 = document.getElementById("colorbutton0");
+let colorButton1 = document.getElementById("colorbutton1");
+let colorButton2 = document.getElementById("colorbutton2");
+let colorButton3 = document.getElementById("colorbutton3");
 
 for (let i = 0; i < 4; i++) {
   let red = Math.floor(Math.random() * 256);
@@ -9,13 +11,46 @@ for (let i = 0; i < 4; i++) {
   let blue = Math.floor(Math.random() * 256);
   colors.push(`rgb(${red}, ${green}, ${blue})`);
 }
+let correctAnswerNumber = Math.floor(Math.random() * colors.length);
+
 for (let i = 1; i <= 4; i++) {
   document.documentElement.style.setProperty(`--color-${i}`, colors[i - 1]);
 }
-document.addEventListener("DOMContentLoaded", function () {
-  colorText.textContent = `What color is ${correctAnswerIndex}`;
+let correctAnswer = colors[correctAnswerNumber];
+window.addEventListener("load", () => {
+  colorText.innerHTML = `What color is<br> "${correctAnswer.slice(4, -1)}"?`;
+  console.log("loaded");
 });
 
-//Console Logs
-console.log(correctAnswerNumber);
-console.log(correctAnswerIndex);
+colorButton0.addEventListener("click", function () {
+  if (colors[0] === correctAnswer) {
+    alert("Correct!");
+    location.reload();
+  } else {
+    alert("Wrong!");
+  }
+});
+colorButton1.addEventListener("click", function () {
+  if (colors[1] === correctAnswer) {
+    alert("Correct!");
+    location.reload();
+  } else {
+    alert("Wrong!");
+  }
+});
+colorButton2.addEventListener("click", function () {
+  if (colors[2] === correctAnswer) {
+    alert("Correct!");
+    location.reload();
+  } else {
+    alert("Wrong!");
+  }
+});
+colorButton3.addEventListener("click", function () {
+  if (colors[3] === correctAnswer) {
+    alert("Correct!");
+    location.reload();
+  } else {
+    alert("Wrong!");
+  }
+});
